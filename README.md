af23d9931d5fef48d59c6e71746487889b41bbda
xgangzhao <zsgjmail@126.com>
2025-03-24 21:44:52 +0800

    Create repo and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# A simple encoder and decoder for PNG
+
+TODO!
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod chunk_type;
+
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()> {
+    todo!()
+}
+

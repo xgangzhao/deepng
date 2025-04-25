5af595ad046c71b1fce591e30975504601682900
xgangzhao <zsgjmail@126.com>
2025-04-25 10:22:35 +0800

    main: add message file option

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ mod png;
 mod chunk_type_error;
 mod chunk_error;
 mod png_error;
-use clap::{arg, command, value_parser, Arg, ArgAction, ArgGroup, Command};
-use std::{fs, io::Write, process::Output, str::FromStr};
+use clap::{arg, value_parser, Arg, ArgAction, ArgGroup, Command};
+use std::{fs, io::Write, str::FromStr};
 use std::path::PathBuf;
 
 pub type Error = Box<dyn std::error::Error>;
@@ -22,11 +22,10 @@ fn main() -> Result<()> {
                                             .value_parser(value_parser!(String));
     let msg_file = Arg::new("msg_file").short('M').long("message-file")
                                            .help("The file contains content to be encoded into the PNG file")
-                                           .required(true)
                                            .value_name("FILE")
                                            .action(ArgAction::Set)
                                            .value_parser(value_parser!(PathBuf));
-    let message = arg!(-m --message "The message to encode into the PNG file").required(true)
+    let message = arg!(-m --message "The message to encode into the PNG file")
                                             .value_name("TEXT")
                                             .action(ArgAction::Set)
                                             .value_parser(value_parser!(String));
@@ -46,6 +45,7 @@ fn main() -> Result<()> {
     let decode = Command::new("decode")
                          .arg(input)
                          .arg(ck_type)
+                         .arg(msg_file)
                          .arg(output);
 
     let matches = Command::new("deepng")
@@ -58,7 +58,13 @@ fn main() -> Result<()> {
         Some(("encode", encode)) => {
             let in_file = encode.get_one::<PathBuf>("file").unwrap();
             let ck_type = encode.get_one::<String>("type").unwrap();
-            let message = encode.get_one::<String>("message").unwrap();
+            // let messages = encode.get_one::<PathBuf>("messages");
+            let mut data = "".to_string();
+            if let Some(msg_file) = encode.get_one::<PathBuf>("msg_file") {
+                data = fs::read_to_string(msg_file)?;
+            } else if let Some(message) = encode.get_one::<String>("message") {
+                data = message.to_string();
+            }
             let out_file = encode.get_one::<PathBuf>("out");
             let content = fs::read(&in_file)?;
             let mut png = png::Png::try_from(content.as_ref())?;
@@ -66,7 +72,8 @@ fn main() -> Result<()> {
             if ck_type.is_valid_type() == false {
                 return Err(Box::from(png_error::PngError::InvalidEncodeType));
             }
-            let ck = chunk::Chunk::new(ck_type, message.as_bytes().to_vec());
+            let ck = chunk::Chunk::new(ck_type, data.as_bytes().to_vec());
+
             png.append_chunk(ck);
             match out_file {
                 Some(out_file) => {
@@ -79,29 +86,40 @@ fn main() -> Result<()> {
                 }
             }
         }
+
         Some(("decode", decode)) => {
             let in_file = decode.get_one::<PathBuf>("file").unwrap();
             let ck_type = decode.get_one::<String>("type").unwrap();
+            let msg_file = decode.get_one::<PathBuf>("msg_file");
             let out_file = decode.get_one::<PathBuf>("out");
             let content = fs::read(in_file)?;
             let mut png = png::Png::try_from(content.as_ref())?;
             let ck = png.remove_last_chunk(ck_type);
             match ck {
-                Ok(ck) => {   
-                    match out_file {
-                        Some(out_file) => {
-                            let msg = ck.data();
-                            let mut ofile = fs::File::create(&out_file)?;
-                            ofile.write_all(msg)?;
+                Ok(ck) => {
+                    match msg_file {
+                        Some(msg_file) => {
+                            let mut ofile = fs::File::create(&msg_file)?;
+                            ofile.write_all(ck.data())?;
                         }
                         None => {
-                            let msg = ck.data_as_string()?;
-                            println!("{}", msg);
+                            println!("{}", ck.data_as_string()?);
                         }
                     }
+
                 }
                 Err(e) => {
                     eprintln!("Error: {}", e);
+                }
+            }
+            match out_file {
+                Some(out_file) => {
+                    let mut ofile = fs::File::create(&out_file)?;
+                    ofile.write_all(&png.as_bytes())?;
+                }
+                None => {
+                    let mut ofile = fs::File::create(&in_file)?;
+                    ofile.write_all(&png.as_bytes())?;
                 }
             }
         }
